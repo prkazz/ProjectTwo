120b9313267ff1bb9819ed429c60be40ccf52b69
prkazz <124136966+prkazz@users.noreply.github.com>
2023-02-06 19:01:17 +0530

    Update GhubPractice.java

--- src/test/java/gitHubCheck/GhubPractice.java
+++ src/test/java/gitHubCheck/GhubPractice.java
@@ -8,9 +8,9 @@ public class GhubPractice {
 		System.out.println("hello world");
 		System.out.println("good evening");
 		System.out.println("good day");
-		System.out.println("good day");
-		System.out.println("good day");
-		System.out.println("good day");
+		System.out.println("good 1");
+		System.out.println("good 2");
+		System.out.println("good 3");
 		
 	} 
 
